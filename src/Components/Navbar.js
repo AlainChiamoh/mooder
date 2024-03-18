f5350799f237a78e3d9bfe16ecdb34436102682b
Frank <alainchiamoh@gmail.com>
2024-03-18 07:19:59 -0700

    Initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const container =styled.div`
+
+`
+
+
+const Navbar = () => {
+  return (
+    <container>Navbar</container>
+  )
+}
+
+export default Navbar
